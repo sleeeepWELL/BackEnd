03cf648e056f652183823777a4ae74b04d26b56a
kellykang-tech <kellykang13@gmail.com>
2021-04-26 11:19:51 +0900

    config setting.

---
+++ src/main/java/project/sleepwell/SleepwellApplication.java
@@ -0,0 +1,13 @@
+package project.sleepwell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepwellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SleepwellApplication.class, args);
+    }
+
+}
