e8c0c11f78d7259f757651554c490b79afb3d662
wanjongth <53491653+wanjongth@users.noreply.github.com>
2021-05-06 15:46:02 +0900

    add barchart API - tags counting

--- src/main/java/project/sleepwell/domain/cards/CardsRepository.java
+++ src/main/java/project/sleepwell/domain/cards/CardsRepository.java
@@ -11,14 +11,22 @@ import java.util.List;
 // 확장하게되어 도메인 별 프로젝트를 분리해야 할 때가 오면 Entity 와 Repository 는 같이 움직어야 하므로 도메인 패키지에서 함께 관리
 public interface CardsRepository extends JpaRepository<Cards, Long> { // <Entity Class, PK type> -> 기본적인 CRUD 메서드 자동으로 생성
 
+    // 모두 찾기 - 현재 안씀
     List<Cards> findAll();
 
+    // 상세 조회때 쓰는 유저별 selectedAt에 해당하는 카드 조회
     Cards findCardsBySelectedAtEqualsAndUser(LocalDate selectedAt, User user);
 
+    // 유저별 카드 전체 조회
     List<Cards> findCardsByUser(User user);
 
+    // 유저별 카드 삭제
     String deleteCardsBySelectedAtEqualsAndUser(LocalDate selectedAt, User user);
 
+    // 유저별 적정 수면시간 구할 때 쓰는 카드 조회
     List<Cards> findCardsByConditionsGreaterThanAndUserEquals(Long conditions, User user);
+
+    // 유저별 태그의 빈도수 조회
+    List<Cards> findCardsBySelectedAtIsAfterAndUser(LocalDate today, User user);
 
 }
--- src/main/java/project/sleepwell/web/ChartController.java
+++ src/main/java/project/sleepwell/web/ChartController.java
@@ -1,11 +1,14 @@
 package project.sleepwell.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import project.sleepwell.service.ChartService;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -18,5 +21,11 @@ public class ChartController {
     @GetMapping("/yourSleepTime")
     public List<Integer> yourSleepTimeByConditions(@AuthenticationPrincipal org.springframework.security.core.userdetails.User principal) {
         return chartService.yoursleeptimebyconditions(principal);
+    }
+
+    // 오늘을 기준으로 주간,월간 태그의 빈도수 구하기 그래프
+    @GetMapping("/barChart/{today}")
+    public List<List<Integer>> tagBarChart (@PathVariable("today") @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate today, @AuthenticationPrincipal org.springframework.security.core.userdetails.User principal) {
+        return chartService.tagbarchart(today,principal);
     }
 }
