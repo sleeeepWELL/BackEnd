3ad7e8f7059322a3612c92f2e06a10e0b97ec73c
wanjongth <53491653+wanjongth@users.noreply.github.com>
2021-05-10 22:21:41 +0900

    fix average

--- src/main/java/project/sleepwell/SleepwellApplication.java
+++ src/main/java/project/sleepwell/SleepwellApplication.java
@@ -2,9 +2,7 @@ package project.sleepwell;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import project.sleepwell.config.AppProperties;
 
 //@EnableConfigurationProperties(AppProperties.class)     //project 에서 사용할 수 있도록 선언
 @EnableJpaAuditing
