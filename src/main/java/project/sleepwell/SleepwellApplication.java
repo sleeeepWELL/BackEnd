556cfc1c04a19563cc323ccc9409157a0f72a7fb
wanjongth <53491653+wanjongth@users.noreply.github.com>
2021-05-25 17:25:30 +0900

    scheduler test

--- src/main/java/project/sleepwell/SleepwellApplication.java
+++ src/main/java/project/sleepwell/SleepwellApplication.java
@@ -3,10 +3,12 @@ package project.sleepwell;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableScheduling
 public class SleepwellApplication {
 
     public static void main(String[] args) {
